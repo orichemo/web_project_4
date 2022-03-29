0f47b1e9bb54d209269cdb330fd0b297628b7650
Ori Chemo <orichemo@gmail.com>
2022-03-29 18:47:58 +0300

    second submit project 4 Ori Hemo

--- scripts/index.js
+++ scripts/index.js
@@ -9,11 +9,11 @@ const inputBreed = document.querySelector(".popup__input_type_about-me");
 function toggleFormOpen() {
   inputName.value = profileName.textContent;
   inputBreed.value = profileBreed.textContent;
-  popup.classList.toggle("popup_opened");
+  popup.classList.toggle("popup_open");
 }
 
 function toggleFormClose() {
-  popup.classList.toggle("popup_opened");
+  popup.classList.toggle("popup_open");
 }
 
 openFormButton.addEventListener("click", toggleFormOpen);
@@ -25,18 +25,7 @@ function handleFormSubmit(e) {
   e.preventDefault();
   profileName.textContent = inputName.value;
   profileBreed.textContent = inputBreed.value;
-  popup.classList.toggle("popup_opened");
+  popup.classList.toggle("popup_open");
 }
 
 form.addEventListener("submit", handleFormSubmit);
-
-let likesButtons = document.querySelectorAll(".element__like-button");
-
-for (let i = 0; i < likesButtons.length; i++) {
-  let likeButton = likesButtons[i];
-  likeButton.classList.remove("element__like-button_staus_active");
-
-  likeButton.addEventListener("click", function () {
-    likeButton.classList.toggle("element__like-button_staus_active");
-  });
-}
